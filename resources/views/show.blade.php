edfc97acde561e8f5739f1c8bba46fca79574a5f
volty <voltyfurr@gmail.com>
2022-09-07 13:27:12 +0300

    Made code cleaner by use view models

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,7 +5,7 @@
     <div class="movie-info border-b border-gray-800">
         <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
             <div class="flex-none">
-                <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$movie['poster_path'] }}" alt="" class="w-64 lg:w-96">
+                <img src="{{ $movie['poster_path'] }}" alt="" class="w-64 lg:w-96">
             </div>
             <div class="md:ml-24">
                 <h2 class="text-4xl font-semibold">{{ $movie['title'] }}</h2>
@@ -18,31 +18,21 @@
                                 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"/>
                         </g>
                     </svg>
-                    <span class="ml-1">{{ $movie['vote_average'] * 10 .'%'}}</span>
+                    <span class="ml-1">{{ $movie['vote_average']}}</span>
                     <span class="mx-2">|</span>
-                    <span>{{ \Carbon\Carbon::parse($movie['release_date'])->format('M d, Y') }}</span>
+                    <span>{{ $movie['release_date'] }}</span>
                     <span class="mx-2">|</span>
-                    <span>
-                        @foreach($movie['genres'] as $genre)
-                            {{ $genre['name'] }}@if(!$loop->last)
-                                ,
-                            @endif
-                        @endforeach
-                    </span>
+                    <span>{{ $movie['genres'] }}</span>
                 </div>
-                <p class="text-gray-300 mt-8">
-                    {{ $movie['overview'] }}
-                </p>
+                <p class="text-gray-300 mt-8">{{ $movie['overview'] }}</p>
                 <div class="mt-8">
                     <h4 class="text-white font-semibold">Featured cast</h4>
                     <div class="flex mt-6">
-                        @foreach($movie['credits']['crew'] as $crew)
-                            @if($loop->index < 2)
-                                <div class="mr-8">
-                                    <div>{{ $crew['name'] }}</div>
-                                    <div class="text-sm text-gray-400">{{ $crew['job'] }}</div>
-                                </div>
-                            @endif
+                        @foreach($movie['crew'] as $crew)
+                            <div class="mr-8">
+                                <div>{{ $crew['name'] }}</div>
+                                <div class="text-sm text-gray-400">{{ $crew['job'] }}</div>
+                            </div>
                         @endforeach
                     </div>
                 </div>
@@ -52,9 +42,8 @@
                 <div x-data="{ isOpen: false }">
                     @if (count($movie['videos']['results']) > 0)
                         <div class="mt-12">
-                            <button
-                                @click="isOpen = true"
-                                class="flex inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
+                            <button @click="isOpen = true"
+                                    class="flex inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
                                 <svg class="w-6 fill-current" viewBox="0 0 24 24">
                                     <path d="M0 0h24v24H0z" fill="none"/>
                                     <path
@@ -72,10 +61,9 @@
                                 <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
                                     <div class="bg-gray-900 rounded">
                                         <div class="flex justify-end pr-4 pt-2">
-                                            <button
-                                                @click="isOpen = false"
-                                                @keydown.escape.window="isOpen = false"
-                                                class="text-3xl leading-none hover:text-gray-300">&times;
+                                            <button @click="isOpen = false"
+                                                    @keydown.escape.window="isOpen = false"
+                                                    class="text-3xl leading-none hover:text-gray-300">&times;
                                             </button>
                                         </div>
                                         <div class="modal-body px-8 py-8">
@@ -104,21 +92,19 @@
         <div class="container mx-auto px-4 py-16">
             <h2 class="text-4xl font-semibold">Cast</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-                @foreach($movie['credits']['cast'] as $cast)
-                    @if($loop->index < 5)
-                        <div class="mt-8">
-                            <a href="#">
-                                <img src="{{ 'https://image.tmdb.org/t/p/w300'.$cast['profile_path'] }}" alt="actor"
-                                     class="hover:opacity-75 transition ease-in-out duration-150">
-                            </a>
-                            <div class="mt-2">
-                                <a href="#" class="text-lg mt-2 hover:text-gray-300">{{  $cast['name'] }}</a>
-                                <div class="text-gray-400 text-sm">
-                                    {{ $cast['character'] }}
-                                </div>
+                @foreach($movie['cast'] as $cast)
+                    <div class="mt-8">
+                        <a href="#">
+                            <img src="{{ 'https://image.tmdb.org/t/p/w300'.$cast['profile_path'] }}" alt="actor"
+                                 class="hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="mt-2">
+                            <a href="#" class="text-lg mt-2 hover:text-gray-300">{{  $cast['name'] }}</a>
+                            <div class="text-gray-400 text-sm">
+                                {{ $cast['character'] }}
                             </div>
                         </div>
-                    @endif
+                    </div>
                 @endforeach
             </div>
         </div>
@@ -129,20 +115,17 @@
         <div class="container mx-auto px-4 py-16">
             <h2 class="text-4xl font-semibold">Pictures</h2>
             <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                @foreach ($movie['images']['backdrops'] as $image)
-                    @if ($loop->index < 9)
-                        <div class="mt-8">
-                            <a
-                                @click.prevent="
+                @foreach ($movie['images'] as $image)
+                    <div class="mt-8">
+                        <a @click.prevent="
                                 isOpen = true
                                 image = '{{ "https://image.tmdb.org/t/p/original/".$image['file_path'] }}'
                                 "
-                                href="#">
-                                <img src="{{ "https://image.tmdb.org/t/p/w500".$image['file_path'] }}" alt="image"
-                                     class="hover:opacity-75 transition ease-in-out duration-150">
-                            </a>
-                        </div>
-                    @endif
+                           href="#">
+                            <img src="{{ "https://image.tmdb.org/t/p/w500".$image['file_path'] }}" alt="image"
+                                 class="hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                    </div>
                 @endforeach
             </div>
 
@@ -153,11 +136,9 @@
                 <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
                     <div class="bg-gray-900 rounded">
                         <div class="flex justify-end pr-4 pt-2">
-                            <button
-                                @click="isOpen = false"
-                                @keydown.escape.window="isOpen = false"
-
-                                class="text-3xl leading-none hover:text-gray-300">&times;
+                            <button @click="isOpen = false"
+                                    @keydown.escape.window="isOpen = false"
+                                    class="text-3xl leading-none hover:text-gray-300">&times;
                             </button>
                         </div>
                         <div class="modal-body px-8 py-8">
